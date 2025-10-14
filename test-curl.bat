52fb715cd19527c96f1196f0582ed438e8f0ab9b
diayal-senegal <papaoumardeme@gmail.com>
2025-10-14 11:24:07 +0200

    Clean up before removing large files

--- test-curl.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Testing PayDunya API...
-curl -X POST http://localhost:5000/api/payment/mobile/initiate ^
-  -H "Content-Type: application/json" ^
-  -d "{\"orderId\":\"TEST-123\",\"amount\":1000,\"customerName\":\"Test User\",\"provider\":\"orange-money-senegal\",\"phoneNumber\":\"+221771234567\"}"
-pause
