eb2b4ac1b748f72db6e468674e55d9c9261074b4
diayal-senegal <papaoumardeme@gmail.com>
2025-11-05 08:55:25 +0100

    Ajout .env.production et mise à jour frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
- const local = 'http://localhost:5000'
- const production = 'https://diayma.com'
+
  const api = axios.create({
-     baseURL: `${local}/api`
+     baseURL: process.env.NODE_ENV === 'production' 
+         ? `${process.env.REACT_APP_API_URL}/api` 
+         : "http://localhost:5000/api"
     });
 
     export default api
