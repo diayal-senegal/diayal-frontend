cd44cfcfc9947ff9f5a40f055ff811398d83d73d
diayal-senegal <papaoumardeme@gmail.com>
2026-01-21 04:48:50 +0100

    feat: Configure PWA pour dashboard et frontend avec icône_diayal.png

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import { Toaster } from 'react-hot-toast';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -25,6 +26,8 @@ root.render(
     </Suspense>
   </Provider>
 );
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
